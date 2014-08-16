61ed59dc15b1839965fac60cd2c8c28598f951fb
Swift Dojo <swiftdojo@MacBook-Pro.local>
2014-08-16 09:47:44 -0300

    Initial Commit

---
+++ CocoaQuiz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CocoaQuiz
+//
+//  Created by Swift Dojo on 16/08/14.
+//  Copyright (c) 2014 Swift Dojo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
